aea90f4621f4cde9bd71bd12389ee60e9ea9aabc
chantake1128 <chantake1128@gmail.com>
2024-08-12 00:55:59 +0900

    ユーザー認証の導入

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-#bundle exec rake db:migrate
-postgresql://ajaxapp_production_dkat_user:DZs9L1Zuye3hudwKVlUSb39TSyFQY1nv@dpg-cqqtnal6l47c73b2j8u0-a.ohio-postgres.render.com/ajaxapp_production_dkat
+bundle exec rake db:migrate
